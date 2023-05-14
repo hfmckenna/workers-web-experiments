45e9b7273a25e10c260ae1d7de1d232cc61944c7
Hugh McKenna <hfmckenna@outlook.com>
2023-05-14 18:02:19 +0100

    chore: Add PUT to test

--- cloud-gallery/header/src/root.tsx
+++ cloud-gallery/header/src/root.tsx
@@ -6,11 +6,23 @@ import { Slider } from "./Slider/Slider";
 export const Header = component$(() => {
 	useStylesScoped$(styles);
 	useFragmentRoot();
-
+	const putRecipe = $(async () => {
+		await fetch('https://author-bff.test.admin.skran.app/v1/recipes', {
+			method: 'PUT',
+			body: JSON.stringify({
+				"detail": {
+					"state": "published"
+				}
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+	})
 	return (
 		<>
 			<Slider />
-
+			<button onClick$={putRecipe}>PUT ME!</button>
 			<div class="container">
 				<Image alt="Cloudflare logo" src="cf-logo.png" width="74" height="35" />
 				<div class="title">Cloud Gallery</div>

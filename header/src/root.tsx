d60fd0c417791fe91be81daab99bc957303d679f
Hugh McKenna <hfmckenna@outlook.com>
2023-06-04 22:07:16 +0100

    chore: Consistent versions

--- header/src/root.tsx
+++ header/src/root.tsx
@@ -1,36 +1,15 @@
-import { $, component$, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import { Image, useFragmentRoot } from "@hfmckenna/skran-app-tools";
 import styles from "./Header.css?inline";
 
 export const Header = component$(() => {
 	useStylesScoped$(styles);
 	useFragmentRoot();
-	const putRecipe = $(async () => {
-		const token = document.cookie.split("; ");
-		const tokenCookie = token.find((row) =>
-			row.startsWith("CF_Authorization=")
-		);
-		const tokenValue = tokenCookie?.split("=")[1];
-		await fetch("https://author-bff.test.admin.skran.app/v1/recipes", {
-			method: "PUT",
-			body: JSON.stringify({
-				detail: {
-					state: "published",
-				},
-			}),
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: "Bearer " + tokenValue,
-			},
-		});
-	});
 	return (
 		<>
 			<div class="container">
 				<Image alt="Cloudflare logo" src="cf-logo.png" width="74" height="35" />
 				<div class="title">Cloud Gallery</div>
-				<button onClick$={putRecipe}>PUT ME!</button>
-
 				<a href="https://github.com/cloudflare/workers-web-experiments/tree/main/qwik-workers">
 					<Image
 						alt="Github icon"
